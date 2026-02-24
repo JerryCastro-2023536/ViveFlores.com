59b687383dbe06c189ce8ecde99657447bdc1f1e
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-02-24 11:44:31 -0600

    feat: Se ha cargado la implementacion de los servicios de la entidad Favoritos

---
+++ FavoritosServiceImplements.java
@@ -0,0 +1,48 @@
+package com.viveflores.blogturistico.Service;
+
+import com.viveflores.blogturistico.Entity.Favoritos;
+import com.viveflores.blogturistico.Repository.FavoritosRepository;
+
+import java.util.List;
+
+public class FavoritosServiceImplements implements FavoritosService{
+    private final FavoritosRepository favoritosRepository;
+
+    public FavoritosServiceImplements(FavoritosRepository favoritosRepository) {
+        this.favoritosRepository = favoritosRepository;
+    }
+
+    @Override
+    public List<Favoritos> getFavoritosAll() {
+        return favoritosRepository.findAll();
+    }
+
+    @Override
+    public Favoritos getFavoritosById(Integer id) {
+        return favoritosRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Favoritos saveFavoritos(Favoritos favoritos) {
+        return favoritosRepository.save(favoritos);
+    }
+
+    @Override
+    public Favoritos updateFavoritos(Integer id, Favoritos favoritos) {
+        Favoritos favoritos1 = favoritosRepository.findById(id).orElse(null);
+        if(favoritos1 != null){
+            favoritos1.setId_usuario(favoritos.getId_usuario());
+            favoritos1.setId_publicacion(favoritos.getId_publicacion());
+            favoritos1.setId_categoria(favoritos.getId_categoria());
+        }else{
+            throw new IllegalArgumentException("El id no existe");
+        }
+        return favoritosRepository.save(favoritos1);
+    }
+
+    @Override
+    public void deleteFavoritos(Integer id) {
+        Favoritos favoritos = favoritosRepository.findById(id).orElse(null);
+        favoritosRepository.delete(favoritos);
+    }
+}
